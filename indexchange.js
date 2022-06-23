4330fdb6936aa0264cac6ff466ec2a077284c043
Darya K <dashakasap@gmail.com>
2022-06-23 16:08:31 +0300

    fix button

--- indexchange.js
+++ indexchange.js
@@ -1,6 +1,7 @@
 
  function MyChange(){
             var u = document.getElementById("change").value
+            var u = u.toUpperCase(u)
             let arr = u.split(' ');
             var requestURL = 'https://api.exchangerate.host/latest?base='+arr[1];
             var request = new XMLHttpRequest();
@@ -9,35 +10,34 @@
            request.send();
            request.onload = function() {
              var response = request.response.rates;
-
-             
+            
+   
              //let keys = Object.keys(response)
     //          let b = Object.entries(response)
     Object.keys(response).forEach((key) => {
         if(key==arr[1]){
-            
+        
+        
     }
-})
+    })
             Object.keys(response).forEach((keys) => {
                 if(keys==arr[3]){
-            console.log(keys)
-                for (let s of Object.entries(response)) {
+                      for (let s of Object.entries(response)) {
                         if(keys==s[0]){
                             let b= arr[0]*s[1]
-                            console.log(b)
                             document.getElementById("res").innerHTML = b;
+                            
                                                   }   
                 
             }
-           
+        
           
                 }
             }) 
             
-        }
+           }
      
 
  
     }
 
- 
